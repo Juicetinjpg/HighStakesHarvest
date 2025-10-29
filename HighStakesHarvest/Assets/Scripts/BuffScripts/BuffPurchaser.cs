2b3ad92999723765e3a346fcff361751a4a3c8d1
starserval <ka070861@ucf.edu>
2025-10-28 21:29:23 -0400

    Update BuffPurchaser.cs

--- HighStakesHarvest/Assets/Scripts/BuffScripts/BuffPurchaser.cs
+++ HighStakesHarvest/Assets/Scripts/BuffScripts/BuffPurchaser.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class BuffPurchaser : MonoBehaviour
 {
-
     [SerializeField] public List<ScriptableBuff> allBuffs = new List<ScriptableBuff>();
+    private BuffManager buffManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        buffManager = GetComponent<BuffManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ApplyRandomBuff()
+    {
+        int randomNum = Random.Range(0, allBuffs.Count);
+        buffManager.AddBuff(allBuffs[randomNum]);
     }
 }
