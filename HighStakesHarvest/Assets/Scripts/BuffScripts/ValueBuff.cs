4c12dcb43a59e24e42ff9bb780a9025d6e499d33
starserval <ka070861@ucf.edu>
2025-11-04 23:59:42 -0500

    Buff additions and fix cropmanager scope

---
+++ HighStakesHarvest/Assets/Scripts/BuffScripts/ValueBuff.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ValueBuff", menuName = "Buffs/ValueBuff")]
+[Serializable]
+public class ValueBuff : ScriptableBuff
+{
+
+    public string cropAffected;
+    public float modifier;
+
+    public override void Apply(GameObject target)
+    {
+        CropManager cropManager = target.GetComponent<CropManager>();
+        CropInfo crop = cropManager.getCropInfo(cropAffected);
+        cropManager.ApplySpecificValueBuff(crop, modifier);
+        Debug.Log($"Crop '{cropAffected}' value is now '{cropManager.getCropValue(cropAffected)}'.");
+    }
+
+    public override void Remove(GameObject target)
+    {
+        throw new System.NotImplementedException();
+    }
+}
